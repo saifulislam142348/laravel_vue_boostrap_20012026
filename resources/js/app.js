193431a52f3a0521b8a64ed1afa18ba2bfb1b082
Md. Saiful islam <72945879+saifulislam142348@users.noreply.github.com>
2026-01-21 13:41:10 +0600

    added

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+import App from './components/App.vue';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+createApp(App).mount('#app');
---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container mt-5">
+        <h1 class="text-primary">Laravel + Vue + Bootstrap</h1>
+        <button class="btn btn-success">Bootstrap Button</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
